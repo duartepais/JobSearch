48b41d1942bef086457222b23b6fc3d7d6c9ce4e
duartepais <duarteavp@gmail.com>
2025-03-31 17:33:09 +0200

    created functions to send emails and to read json files

--- utils.py
+++ utils.py
@@ -2,6 +2,17 @@
 Collection of functions that help the main file and do not fit the model category
 """
 
+import json
+import os
+
+from collections import defaultdict
+
+import requests
+
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
 
 def find_multiple_tags(soup, tag_name, tag_attrs, children_tag_names_list):
     """find the all tags given the different attribute possibilities"""
@@ -38,3 +49,45 @@ def find_single_tag(soup, tag_name, tag_attrs):
         return soup.find(tag_name, {tag_attrs["key"]: True})
 
     return soup.find(tag_name, {tag_attrs["key"]: tag_attrs["value"]})
+
+
+def read_json_file(filename, create_if_inexistent=False):
+    """read a specific file, or create an empty version of it if it does not exist"""
+
+    if create_if_inexistent:
+        try:
+            file_size = os.path.getsize(filename)
+            with open(filename, "r", encoding="utf-8") as read_file:
+                if file_size == 0:
+                    data_dict = defaultdict(dict)
+                else:
+                    data_dict = defaultdict(dict, json.load(read_file))
+
+        except FileNotFoundError:
+            with open(filename, "w", encoding="utf-8"):
+                pass
+            data_dict = defaultdict(dict)
+    else:
+        with open(filename, "r", encoding="utf-8") as read_file:
+            data_dict = json.load(read_file)
+
+    return data_dict
+
+
+def send_email(email_html, email_date):
+    """
+    Send an email with some date on the subject and some html
+    """
+    result = requests.post(
+        f"https://api.mailgun.net/v3/{config["EMAIL_DOMAIN"]}/messages",
+        auth=("api", config["API_KEY"]),
+        data={
+            "from": f"Mailgun Sandbox <mailgun@{config["EMAIL_DOMAIN"]}>",
+            "to": f"<{config["EMAIL_ADDRESS"]}>",
+            "subject": f"New jobs - {email_date}",
+            "html": email_html,
+        },
+        timeout=10,
+    )
+
+    print(result)
