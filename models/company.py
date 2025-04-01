b914e3e10b9ca3304aae373d41b1fcb1a4bb083c
duartepais <duarteavp@gmail.com>
2025-04-01 14:26:37 +0200

    enlongated the waiting time for the chromium pages to load

--- models/company.py
+++ models/company.py
@@ -29,7 +29,7 @@ options.add_argument("--headless")  # Run Chrome in headless mode
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 options.add_argument(f"user-agent={USER_AGENT}")
 
-PAGE_LOADING_TIME = 2
+PAGE_LOADING_TIME = 10
 
 
 class ResultsLoading(Enum):
