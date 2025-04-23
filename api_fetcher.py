e039fa8590c301e20c9fd19ca95255d097e1bca5
duartepais <duarteavp@gmail.com>
2025-04-23 15:40:11 +0200

    created script to fetch api responses

--- api_fetcher.py
+++ api_fetcher.py
@@ -4,8 +4,8 @@ import argparse
 import json
 
 from dotenv import dotenv_values
-from models.company import CompanyScrape
-from utils import kill_chrome_processes, read_json_file
+from models.company import CompanyAPI
+from utils import read_json_file
 
 
 config = dotenv_values(".env")
@@ -25,9 +25,7 @@ def update_today_data(company_name: str, jobs_dict: dict, error_list: list):
 
     # read yesterday's and today's data
     today_dict = read_json_file(config["TODAY_FILENAME"], create_if_inexistent=True)
-    yesterday_dict = read_json_file(
-        config["YESTERDAY_FILENAME"], create_if_inexistent=True
-    )
+    yesterday_dict = read_json_file(config["YESTERDAY_FILENAME"])
 
     # deal with eventual empty results
     if jobs_dict:
@@ -51,18 +49,16 @@ def update_today_data(company_name: str, jobs_dict: dict, error_list: list):
 
 def main():
     """
-    routine to scrape a company's data
+    routine to fetch the company's data from its endpoint
     """
 
     args = parser.parse_args()
 
-    company = CompanyScrape(args.company_dict)
+    company = CompanyAPI(args.company_dict)
 
-    company.scrape_results()
+    company.fetch_results()
 
     update_today_data(company.name, company.jobs_dict, company.errors)
-
-    kill_chrome_processes()
 
 
 if __name__ == "__main__":
