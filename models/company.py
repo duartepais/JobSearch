9d254d6b3fc9084203d6f3d95f989ea21526801c
duartepais <duarteavp@gmail.com>
2025-04-23 15:39:34 +0200

    tweaked Company classes to include API interactions

--- models/company.py
+++ models/company.py
@@ -2,10 +2,11 @@
 Classes for companies' websites and job listings
 """
 
+import json
 import re
 import traceback
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from enum import auto, Enum
 
 from bs4 import BeautifulSoup
@@ -13,12 +14,13 @@ from bs4.element import Tag
 
 from dotenv import dotenv_values
 
+from models.api import IterativeAPIInteraction, SingleAPIInteraction
 from models.browser import (
     SimplePageInteraction,
     LoadMoreInteraction,
     PaginationInteraction,
 )
-from utils import find_multiple_tags, find_single_tag
+from utils import find_multiple_tags, find_single_tag, get_by_path, clean_string
 
 config = dotenv_values(".env")
 
@@ -33,78 +35,38 @@ class ResultsLoading(Enum):
     SIMPLE_SAME_PAGE = auto()
 
 
-class CompanyScrape:
+class APIResponseFormat(Enum):
     """
-    Abstraction for the company website and its scraping
+    Enumeration for the different kinds of API responses
     """
 
-    def __init__(self, company_dict: dict):
+    JSON = "json"
+    HTML = "html"
+    JSON_AND_HTML = "json+html"
+
+
+class CompanyData(ABC):
+    """
+    Base blueprint for a company's data
+    """
+
+    def __init__(self, company_dict, job_container_metadata):
         self.name = company_dict["name"]
         self.url = company_dict["base_url"]
-        self.results_loading = self.get_results_loading_type(company_dict)
-        self.job_container_metadata = JobContainerMetadata(
-            company_dict["job_container"]
-        )
+
+        self.job_container_metadata = job_container_metadata
+
         self.jobs_dict = {}
+
         self.errors = []
-
-    def get_results_loading_type(self, company_dict: dict):
-        """Assess which kind of results loading this website has"""
-
-        if "next_page" in company_dict:
-            self.browser_interaction = PaginationInteraction(
-                self.url, company_dict["next_page"], company_dict["job_container"]
-            )
-            return ResultsLoading.PAGINATION
-        elif "load_more" in company_dict:
-            self.browser_interaction = LoadMoreInteraction(
-                self.url, company_dict["load_more"], company_dict["job_container"]
-            )
-            return ResultsLoading.LOAD_MORE_SAME_PAGE
-        else:
-            self.browser_interaction = SimplePageInteraction(self.url)
-            return ResultsLoading.SIMPLE_SAME_PAGE
-
-    def fetch_results(self):
-        """Get all the job results available in a company's website"""
-
-        try:
-            self.browser_interaction.run()
-        except Exception as error:
-            self.errors.append(" ".join(error.args))
-        else:
-            match self.results_loading:
-                case ResultsLoading.PAGINATION:
-                    html_list = self.browser_interaction.html_list
-                case (
-                    ResultsLoading.LOAD_MORE_SAME_PAGE | ResultsLoading.SIMPLE_SAME_PAGE
-                ):
-                    html_list = [self.browser_interaction.html]
-
-            job_containers = []
-
-            for html in html_list:
-                job_containers.extend(self.extract_jobs_from_html(html))
-
-            ids_set = set([job.id for job in job_containers])
-            refined_ids_set = set([job.refined_id for job in job_containers])
-
-            if len(refined_ids_set) != len(ids_set):
-                self.errors.append(
-                    f"Something wrong with the id refinement of the company {self.name}"
-                )
-
-                self.jobs_dict = {job.id: job.title for job in job_containers}
-            else:
-                self.jobs_dict = {job.refined_id: job.title for job in job_containers}
-
-            if not self.jobs_dict:
-                self.errors.append(
-                    f"No results were retrieved; check the website of {self.name}"
-                )
 
     def extract_jobs_from_html(self, html: str):
         """Get the appropriate job info from the raw html"""
+
+        if isinstance(self.job_container_metadata, JSONJobContainerMetadata):
+            raise ValueError(
+                "For this kind of metadata, you should not be using this method"
+            )
 
         page_soup = BeautifulSoup(html, features="html.parser")
 
@@ -129,8 +91,213 @@ class CompanyScrape:
 
         return job_containers
 
+    def extract_jobs_from_json(self, json_dict: dict):
+        """Get the appropriate job info from the retrieved json"""
 
-class JobContainerMetadata:
+        if isinstance(self.job_container_metadata, HTMLJobContainerMetadata):
+            raise ValueError(
+                "For this kind of metadata, you should not be using this method"
+            )
+
+        job_containers = []
+
+        jobs_list = get_by_path(json_dict, self.job_container_metadata.jobs_path)
+
+        for job_dict in jobs_list:
+            potential_container = JSONJobContainer(
+                self.job_container_metadata, job_dict
+            )
+            if potential_container.has_valid_country():
+
+                job_containers.append(potential_container)
+
+        return job_containers
+
+
+class CompanyAPI(CompanyData):
+    """
+    Abstraction for the company's API
+    """
+
+    def __init__(self, company_dict: dict):
+
+        self.response_format = APIResponseFormat(company_dict["response"]["type"])
+        metadata = self.get_job_container_metadata(company_dict)
+
+        super().__init__(company_dict, metadata)
+
+        self.api_interaction = (
+            IterativeAPIInteraction(company_dict)
+            if "pagination" in company_dict
+            else SingleAPIInteraction(company_dict)
+        )
+
+    def get_job_container_metadata(self, company_dict: dict):
+        """Retrieve the appropriate metadata for the job container"""
+
+        match self.response_format:
+            case APIResponseFormat.JSON:
+                metadata = JSONJobContainerMetadata(
+                    company_dict["response"]["job_container"]
+                )
+            case APIResponseFormat.HTML:
+                metadata = HTMLJobContainerMetadata(
+                    company_dict["response"]["job_container"]
+                )
+
+            case APIResponseFormat.JSON_AND_HTML:
+                metadata = HTMLJobContainerMetadata(
+                    company_dict["response"]["job_container"]
+                )
+                self.html_loc = company_dict["response"]["html_loc"]
+
+        return metadata
+
+    def fetch_results(self):
+        """Fetch the data from the endpoint"""
+
+        job_containers = []
+
+        if isinstance(self.api_interaction, IterativeAPIInteraction):
+
+            job_nr_old = len(job_containers)
+
+            while True:
+                response = self.api_interaction.fetch_response()
+                job_containers.extend(self.extract_jobs(response))
+
+                job_nr_new = len(job_containers)
+
+                if job_nr_new == job_nr_old:
+                    break
+
+                self.api_interaction.increase_stage_number()
+                job_nr_old = job_nr_new
+
+        else:
+            response = self.api_interaction.fetch_response()
+            job_containers.extend(self.extract_jobs(response))
+
+        self.jobs_dict = {
+            job.refined_id if hasattr(job, "refined_id") else job.job_id: job.title
+            for job in job_containers
+        }
+
+        if not self.jobs_dict:
+            self.errors.append(
+                f"No results were retrieved; check the website of {self.name}"
+            )
+
+    def extract_jobs(self, response: str):
+        """Extract jobs from generic response"""
+
+        match self.response_format:
+            case APIResponseFormat.JSON:
+                response_dict = json.loads(response)
+
+                return self.extract_jobs_from_json(response_dict)
+
+            case APIResponseFormat.JSON_AND_HTML:
+                response_dict = json.loads(response)
+                response_html = get_by_path(response_dict, self.html_loc)
+
+                return self.extract_jobs_from_html(response_html)
+
+            case APIResponseFormat.HTML:
+                response_html = response
+
+                return self.extract_jobs_from_html(response_html)
+
+
+class CompanyScrape(CompanyData):
+    """
+    Abstraction for the company website and its scraping
+    """
+
+    def __init__(self, company_dict: dict):
+
+        metadata = HTMLJobContainerMetadata(company_dict["job_container"])
+        super().__init__(company_dict, metadata)
+
+        self.results_loading = self.get_results_loading_type(company_dict)
+
+    def get_results_loading_type(self, company_dict: dict) -> ResultsLoading:
+        """Assess which kind of results loading this website has"""
+
+        if "next_page" in company_dict:
+            self.browser_interaction = PaginationInteraction(
+                self.url, company_dict["next_page"], company_dict["job_container"]
+            )
+            return ResultsLoading.PAGINATION
+        elif "load_more" in company_dict:
+            self.browser_interaction = LoadMoreInteraction(
+                self.url, company_dict["load_more"], company_dict["job_container"]
+            )
+            return ResultsLoading.LOAD_MORE_SAME_PAGE
+        else:
+            self.browser_interaction = SimplePageInteraction(self.url)
+            return ResultsLoading.SIMPLE_SAME_PAGE
+
+    def scrape_results(self):
+        """Get all the job results available in a company's website"""
+
+        try:
+            self.browser_interaction.run()
+        except Exception as error:
+            self.errors.append(" ".join(error.args))
+        else:
+            match self.results_loading:
+                case ResultsLoading.PAGINATION:
+                    html_list = self.browser_interaction.html_list
+                case (
+                    ResultsLoading.LOAD_MORE_SAME_PAGE | ResultsLoading.SIMPLE_SAME_PAGE
+                ):
+                    html_list = [self.browser_interaction.html]
+
+            job_containers = []
+
+            for html in html_list:
+                job_containers.extend(self.extract_jobs_from_html(html))
+
+            ids_set = {job.job_id for job in job_containers}
+            refined_ids_set = {job.refined_id for job in job_containers}
+
+            if len(refined_ids_set) != len(ids_set):
+                self.errors.append(
+                    f"Something wrong with the id refinement of the company {self.name}"
+                )
+
+                self.jobs_dict = {job.job_id: job.title for job in job_containers}
+            else:
+                self.jobs_dict = {job.refined_id: job.title for job in job_containers}
+
+            if not self.jobs_dict:
+                self.errors.append(
+                    f"No results were retrieved; check the website of {self.name}"
+                )
+
+
+class JSONJobContainerMetadata:
+    """
+    Abstraction containing the JSON paths associated with a job container
+    """
+
+    def __init__(self, info_dict: dict):
+        self.jobs_path = info_dict["loc"]
+        self.title_path = info_dict["title"]["loc"]
+        self.id_path = info_dict["id"]["loc"]
+
+        self.country_path = (
+            None if "country" not in info_dict else info_dict["country"]["loc"]
+        )
+        self.country_target = (
+            None
+            if "country" not in info_dict
+            else info_dict["country"]["correct_value"]
+        )
+
+
+class HTMLJobContainerMetadata:
     """
     Abstraction containing the important html tags associated with a job container
     """
@@ -181,17 +348,73 @@ class JobContainerMetadata:
             )
 
 
-class HTMLJobContainer:
+class JobContainer(ABC):
+    """
+    Blueprint class for a job container
+    """
+
+    def __init__(self, job_id, title):
+        self.job_id = job_id
+        self.title = title
+
+    def to_dict(self):
+        """
+        Get a representable dict of the job
+        """
+
+        return {self.job_id: self.title}
+
+
+class JSONJobContainer(JobContainer):
+    """
+    Abstraction containing the selected info of a JSON job container
+    """
+
+    def __init__(
+        self, container_metadata: JSONJobContainerMetadata, container_dict: dict
+    ):
+
+        self.metadata = container_metadata
+
+        self.title = get_by_path(container_dict, self.metadata.title_path)
+        self.job_id = get_by_path(container_dict, self.metadata.id_path)
+
+        self.country = (
+            get_by_path(container_dict, self.metadata.country_path).lower()
+            if (self.metadata.country_path and self.metadata.country_target)
+            else None
+        )
+
+        super().__init__(self.job_id, self.title)
+
+    def has_valid_country(self) -> bool:
+        """
+        check if the country where this job is is valid
+        """
+        if self.country:
+            is_country_valid = (
+                self.metadata.country_target.strip().lower() == self.country
+            )
+            return is_country_valid
+
+        return True
+
+
+class HTMLJobContainer(JobContainer):
     """
     Abstraction containing the selected info of an html job container
     """
 
-    def __init__(self, container_metadata: JobContainerMetadata, container_soup: Tag):
+    def __init__(
+        self, container_metadata: HTMLJobContainerMetadata, container_soup: Tag
+    ):
 
         self.metadata = container_metadata
         self.title = self.get_title(container_soup)
-        self.id = self.get_id(container_soup)
+        self.job_id = self.get_job_id(container_soup)
         self.refined_id = self.get_refined_id()
+
+        super().__init__(self.job_id, self.title)
 
     def get_title(self, soup: Tag):
         """
@@ -209,11 +432,7 @@ class HTMLJobContainer:
 
             title_raw_string = title_soup.text
 
-        title_processed_string = " ".join(
-            title_raw_string.replace("\xa0", " ").splitlines()
-        ).strip()
-
-        title_processed_string = " ".join(title_processed_string.split())
+        title_processed_string = clean_string(title_raw_string)
 
         if not title_processed_string:
             raise ValueError(
@@ -222,7 +441,7 @@ class HTMLJobContainer:
 
         return title_processed_string
 
-    def get_id(self, soup: Tag):
+    def get_job_id(self, soup: Tag):
         """
         Fetch id from soup object, if available
         Input: container soup object
@@ -259,13 +478,6 @@ class HTMLJobContainer:
         """
 
         if self.metadata.id_regex:
-            return re.search(self.metadata.id_regex, self.id).group(1)
-        else:
-            return self.id
+            return re.search(self.metadata.id_regex, self.job_id).group(1)
 
-    def to_dict(self):
-        """
-        Get a representable dict of the job
-        """
-
-        return {self.refined_id: self.title}
+        return self.job_id
